842eac50994436392b6c76148b6af0a71138ec65
weijue <86465502@qq.ccom>
2019-06-29 23:31:32 +0800

    create

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { GlobalStyled } from './styled'
+
+import Header from './common/header'
+
+function App() {
+    return (
+        <div className = "App" >
+            <GlobalStyled/>
+            <Header>
+
+            </Header>
+        </div>
+    );
+}
+
+export default App;
